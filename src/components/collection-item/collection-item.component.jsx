a3a76053b23df506b8013267e8a7e5ca1fd92eda
mr-l0n3lly <andreipatan@live.com>
2021-11-10 00:37:11 +0200

    fixed a bug with cart-dropdown and created cart component with redux

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addItemToCart } from '../../redux/cart/cart.actions';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className='collection-item'>
-    <div
-      className='image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    />
-    <div className='collection-footer'>
-      <span className='name'>{name}</span>
-      <span className='price'>{price}</span>
-    </div>
-  </div>
-);
+const CollectionItem = ({ item, addItemToCart }) => {
+	const {imageUrl, name, price} = item;
+	return (
+		<div className='collection-item'>
+			<div
+				className='image'
+				style={{
+					backgroundImage: `url(${imageUrl})`
+				}}
+			/>
+			<div className='collection-footer'>
+				<span className='name'>{name}</span>
+				<span className='price'>{price}</span>
+			</div>
+			<CustomButton onClick={() => addItemToCart(item)} inverted>ADD TO CART</CustomButton>
+		</div>
+	);
+}
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+	addItemToCart: item => dispatch(addItemToCart(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
